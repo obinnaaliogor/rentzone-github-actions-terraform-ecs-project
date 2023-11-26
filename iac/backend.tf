f45bb564b6f2b6bb15609941d8f50dbae5f011e2
obinnaaliogor <wiz.obi7509@gmail.com>
2023-11-26 01:27:24 +0100

    updated terraform code

---
+++ iac/backend.tf
@@ -0,0 +1,9 @@
+# store the terraform state file in s3 and lock with dynamodb
+terraform {
+  backend "s3" {
+    bucket         = "wiz-terraform-state-file"
+    key            = "terraform-git-action/rentzone/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+  }
+}
